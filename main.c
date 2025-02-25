5e1b8ca49b978ba547c21f75b4e1b034e9363682
nicolasque <quequecedo@gmail.com>
2025-02-25 01:29:36 +0100

    somechanges

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 14:26:00 by nquecedo          #+#    #+#             */
-/*   Updated: 2025/02/22 19:38:52 by nquecedo         ###   ########.fr       */
+/*   Updated: 2025/02/24 13:03:49 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,62 +14,53 @@
 #include <pthread.h>
 #include <sys/time.h>
 
+pthread_mutex_t	mutex;
+int				contador = 0;
 
-pthread_mutex_t mutex;
-int contador = 0;
+void	*hilo_funcion(void *arg)
+{
+	int		j;
+	double	elapsed;
 
-void *hilo_funcion(void *arg) {
-    int j = 0;
-    long long sum = 0;  // Usamos operaciones matemáticas en lugar de memoria
-    struct timeval start, end;
-    long seconds, microseconds;
-    double elapsed;
-    
-    (void)arg;
-    
-    gettimeofday(&start, NULL);
-    
-    // Cálculo intensivo sin operaciones de memoria
-    for (long i = 0; i < 20000000000; i++)
-    {
-        sum += j * j;  // Operación matemática simple
-        j++;
-    }
-    
-    gettimeofday(&end, NULL);
-    
-    seconds = end.tv_sec - start.tv_sec;
-    microseconds = end.tv_usec - start.tv_usec;
-    elapsed = seconds + microseconds/1000000.0;
-    
-    pthread_mutex_lock(&mutex);
-    printf("Hilo %lu completó el cálculo en %.2f segundos (suma: %lld)\n", 
-           (unsigned long)pthread_self(), elapsed, sum);
-    pthread_mutex_unlock(&mutex);
-    
-    return NULL;
+	j = 0;
+	long long sum = 0; // Usamos operaciones matemáticas en lugar de memoria
+	struct timeval start, end;
+	long seconds, microseconds;
+	(void)arg;
+	gettimeofday(&start, NULL);
+	// Cálculo intensivo sin operaciones de memoria
+	for (long i = 0; i < 20000000000; i++)
+	{
+		sum += j * j; // Operación matemática simple
+		j++;
+	}
+	gettimeofday(&end, NULL);
+	seconds = end.tv_sec - start.tv_sec;
+	microseconds = end.tv_usec - start.tv_usec;
+	elapsed = seconds + microseconds / 1000000.0;
+	pthread_mutex_lock(&mutex);
+	printf("Hilo %lu completó el cálculo en %.2f segundos (suma: %lld)\n",
+		(unsigned long)pthread_self(), elapsed, sum);
+	pthread_mutex_unlock(&mutex);
+	return (NULL);
 }
 
+int	main(void)
+{
+	int				nbr_hilos;
+	nbr_hilos = 8;
+	pthread_t		hilos[nbr_hilos];
+	struct timeval	tv;
 
-
-int main() {
-	int nbr_hilos = 8;
-    pthread_t hilos[nbr_hilos];
-	struct timeval tv;
-
-    gettimeofday(&tv, NULL);
-    for (int i = 0; i < nbr_hilos; i++)
+	gettimeofday(&tv, NULL);
+	for (int i = 0; i < nbr_hilos; i++)
 		hilos[i] = (pthread_t)NULL;
-    printf("Segundos: %ld, Microsegundos: %d\n", tv.tv_sec, tv.tv_usec);
-
-
-    pthread_mutex_init(&mutex, NULL);
-    for (int i = 0; i < nbr_hilos; i++)
-        pthread_create(&hilos[i], NULL, hilo_funcion, NULL);
-    
-    for (int i = 0; i < nbr_hilos; i++)
-        pthread_join(hilos[i], NULL);
-
-    pthread_mutex_destroy(&mutex);
-    return 0;
+	printf("Segundos: %ld, Microsegundos: %d\n", tv.tv_sec, tv.tv_usec);
+	pthread_mutex_init(&mutex, NULL);
+	for (int i = 0; i < nbr_hilos; i++)
+		pthread_create(&hilos[i], NULL, hilo_funcion, NULL);
+	for (int i = 0; i < nbr_hilos; i++)
+		pthread_join(hilos[i], NULL);
+	pthread_mutex_destroy(&mutex);
+	return (0);
 }
