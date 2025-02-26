92ecea8c449119d3de19cb3b5d432dae7ab6c42a
nicolasque <quequecedo@gmail.com>
2025-02-26 01:25:43 +0100

    some changes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 14:26:00 by nquecedo          #+#    #+#             */
-/*   Updated: 2025/02/24 13:03:49 by nquecedo         ###   ########.fr       */
+/*   Updated: 2025/02/26 01:23:41 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,17 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-pthread_mutex_t	mutex;
-int				contador = 0;
-
-void	*hilo_funcion(void *arg)
+int	main(int argc, char **argv)
 {
-	int		j;
-	double	elapsed;
-
-	j = 0;
-	long long sum = 0; // Usamos operaciones matemáticas en lugar de memoria
-	struct timeval start, end;
-	long seconds, microseconds;
-	(void)arg;
-	gettimeofday(&start, NULL);
-	// Cálculo intensivo sin operaciones de memoria
-	for (long i = 0; i < 20000000000; i++)
+	int i = 0;
+	if (argc < 4)
 	{
-		sum += j * j; // Operación matemática simple
-		j++;
+		return (1);
 	}
-	gettimeofday(&end, NULL);
-	seconds = end.tv_sec - start.tv_sec;
-	microseconds = end.tv_usec - start.tv_usec;
-	elapsed = seconds + microseconds / 1000000.0;
-	pthread_mutex_lock(&mutex);
-	printf("Hilo %lu completó el cálculo en %.2f segundos (suma: %lld)\n",
-		(unsigned long)pthread_self(), elapsed, sum);
-	pthread_mutex_unlock(&mutex);
-	return (NULL);
-}
-
-int	main(void)
-{
-	int				nbr_hilos;
-	nbr_hilos = 8;
-	pthread_t		hilos[nbr_hilos];
-	struct timeval	tv;
-
-	gettimeofday(&tv, NULL);
-	for (int i = 0; i < nbr_hilos; i++)
-		hilos[i] = (pthread_t)NULL;
-	printf("Segundos: %ld, Microsegundos: %d\n", tv.tv_sec, tv.tv_usec);
-	pthread_mutex_init(&mutex, NULL);
-	for (int i = 0; i < nbr_hilos; i++)
-		pthread_create(&hilos[i], NULL, hilo_funcion, NULL);
-	for (int i = 0; i < nbr_hilos; i++)
-		pthread_join(hilos[i], NULL);
-	pthread_mutex_destroy(&mutex);
+	while (i < argc)
+	{
+		ft_printf("ARGV: %s\n", argv[i]);
+		i++;
+	}
 	return (0);
 }
